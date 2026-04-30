7daf89bab67b09cba67b6ac628a80c02355cdd39
HassanAlmaliki <almalikihasan77@gmail.com>
2026-04-30 14:36:19 -0700

    Add fallback routes for register and login in web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
